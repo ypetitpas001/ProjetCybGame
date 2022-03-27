71292627571413e9549e98ebe9fc50b6eae77d8c
ypetitpas001 <ypetitpas001@ensc.fr>
2022-03-27 21:31:58 +0200

    text input + image

--- pages/Enigme1.tsx
+++ pages/Enigme1.tsx
@@ -1,8 +1,13 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image, Button, Alert, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Button, Alert, TextInput, TouchableHighlight } from 'react-native';
 import React from 'react';
 
-export default function Enigme1() {
+interface Enigme1Props {
+    navigation: any;
+}
 
+export default function Enigme1(props: Enigme1Props) {
+
+    const [commande, setCommande] = React.useState("");
 
     return (
         <View style={styles.container}>
@@ -17,18 +22,33 @@ export default function Enigme1() {
             <View>
                 <Text style={styles.texte1}>Tu vas devoir recueillir des informations importantes pour débloquer la situation, tu peux interagir avec les éléments en cliquant dessus</Text>
                 <Text style={styles.texte2}>Voici le tableau de bord</Text>
-                <Image
-                    style={styles.tableaudebord}
-                    source={require("../assets/tableaudebord.png")}
-                />
+                <TouchableHighlight onPress={() => props.navigation.navigate('Enigme1_2')}>
+                    <Image
+                        style={styles.tableaudebord}
+                        source={require("../assets/tableaudebord.png")}
+                    />
+                </TouchableHighlight>
                 <Text style={styles.texte3}>et voici la console</Text>
             </View>
 
             <View>
                 <TextInput
                     style={styles.input}
-                    value={'essai : '}
+                    placeholderTextColor='green'
                     placeholder="Commandes"
+
+                    onChangeText={(value) => setCommande(value)}
+                    onSubmitEditing={() => {
+                        if (commande == "oui") {
+                            alert(`la commande est : ${commande}`);
+                        }
+                        else {
+                            alert(`la commande n'est pas bonne`);
+                        }
+                        setCommande("");
+                    }}
+                    value={commande}
+
                 />
             </View>
 
@@ -62,11 +82,13 @@ const styles = StyleSheet.create({
     },
 
     texte1: {
+        marginLeft: 5,
         marginTop: 25,
         color: '#fff',
         fontSize: 14,
     },
     texte2: {
+        marginLeft: 5,
         marginTop: 35,
         color: '#fff',
         fontSize: 14,
@@ -88,9 +110,11 @@ const styles = StyleSheet.create({
     },
 
     input: {
-        backgroundColor: "white",
+        backgroundColor: "black",
         height: 100,
-        width: 350,
+        width: 325,
+        borderWidth: 2,
+        borderColor: "#fff",
     },
 
     but: {
--- pages/appNavigator.tsx
+++ pages/appNavigator.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Accueil from './Accueil';
 import Accueil2 from './Accueil2';
 import Enigme1 from './Enigme1';
+import Enigme1_2 from './Enigme1_2';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ const AppNavigator = () => (
         <Screen name="Accueil" component={Accueil} ></Screen>
         <Screen name="Accueil2" component={Accueil2} ></Screen>
         <Screen name="Enigme1" component={Enigme1} ></Screen>
+        <Screen name="Enigme1_2" component={Enigme1_2} ></Screen>
     </Navigator >
 
 
